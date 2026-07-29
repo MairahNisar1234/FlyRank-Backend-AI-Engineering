0ee2ab9dbef79e2c1398ffda8f7f2dc27bda81de
MairahNisar1234 <143585976+MairahNisar1234@users.noreply.github.com>
2026-07-29 17:18:54 +0500

    CRUD api

---
+++ Assignment-2/database.py
@@ -0,0 +1,5 @@
+from sqlmodel import SQLModel, create_engine 
+DATABASE_URL= "sqlite:///tasks.db"
+engine= create_engine(DATABASE_URL, echo=True)
+def create_db():
+    SQLModel.metadata.create_all(engine)
