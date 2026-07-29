f7be125031c772336d38d7b59af473708bc58657
MairahNisar1234 <143585976+MairahNisar1234@users.noreply.github.com>
2026-07-29 17:03:49 +0500

    Containerize your stack

--- Assignment-2/database.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import SQLModel, create_engine 
-DATABASE_URL= "sqlite:///tasks.db"
-engine= create_engine(DATABASE_URL, echo=True)
-def create_db():
-    SQLModel.metadata.create_all(engine)
